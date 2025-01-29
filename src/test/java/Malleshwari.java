0eeba4d9c1f47d164ab4e28ff356d1d5006427b1
Malleshwari4 <pallikondamalleshwari@gmail.com>
2025-01-29 15:48:14 +1300

    added details

---
+++ src/test/java/Malleshwari.java
@@ -0,0 +1,9 @@
+
+public class Malleshwari {
+
+	public static void main(String[] args) {
+		System.out.println("This is Malleshwari");
+		System.out.println("My 10th class percentage is 80%");
+	}
+
+}
