89787b7249660a0edc29aadc45f2e1c0ec94d24c
rajanikumari180681 <rajanikumari180681@gmail.com>
2025-01-29 15:19:44 +0530

    Tenth marks

---
+++ src/test/java/Tenth_Details.java
@@ -0,0 +1,11 @@
+
+public class Tenth_Details {
+
+	public static void main(String[] args) {
+	int percentage;
+		System.out.println("This is Rajani Kumari");
+		System.out.println("My 10th class percentage is 70%");
+		
+	}
+
+}
